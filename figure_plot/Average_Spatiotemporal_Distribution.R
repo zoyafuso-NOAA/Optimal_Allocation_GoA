9b6d1dac92e4dea0b5ab984102832d1cad3d5539
Zack Oyafuso <zackoyafuso@Zacks-MacBook-Air.local>
2020-06-21 08:54:20 -0700

    updated

--- figure_plot/Average_Spatiotemporal_Distribution.R
+++ figure_plot/Average_Spatiotemporal_Distribution.R
@@ -14,7 +14,7 @@ library(rnaturalearth)
 #######################################
 ## Set up directories
 #######################################
-which_machine = c('Zack_MAC' = 1, 'Zack_PC' = 2, 'Zack_GI_PC' = 3)[3]
+which_machine = c('Zack_MAC' = 1, 'Zack_PC' = 2, 'Zack_GI_PC' = 3)[1]
 
 VAST_model = "6g"
 VAST_dir = paste0(c('/Users/zackoyafuso/Google Drive/', 
@@ -43,12 +43,12 @@ data(coastlineWorld)
 data(coastlineWorldFine, package="ocedata")
 
 #Extract USA for AK land
-data(countriesHigh)
-AK = sp::spTransform(countriesHigh[c(39,236),], 
-                     crs('+proj=utm +zone=5 +units=km'))
+# data(countriesHigh)
+# AK = sp::spTransform(countriesHigh[c(39,236),], 
+#                      crs('+proj=utm +zone=5 +units=km'))
 
 AK <- rnaturalearth::ne_countries(continent = "north america", scale = "medium", returnclass = "sp")
-AK <- sp::spTransform(AK, CRS = "+proj=utm +zone=5 +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+AK <- sp::spTransform(AK, CRS = "+proj=utm +zone=5 +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
 
 #######################################
 ## Load Fit, spatial data
@@ -76,6 +76,7 @@ Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
 Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
 
 ns = Save$TmbData$n_c #Number of Species
+sci_names = c(Save$Spp, 'Bathymetry (meters)')
 
 #######################################
 ## Create 200 m isobar
@@ -87,7 +88,6 @@ bathy_ras = raster(bathy, resolution = 5)
 bathy_ras = rasterize(x = bathy, y = bathy_ras, field = 'val')
 
 bathy_iso_200 = raster::rasterToContour(x = bathy_ras, levels = c(200))
-
 
 #######################################
 ## Extract Densities and Calculate Mean Density across years
@@ -105,10 +105,10 @@ mean_density_gc = apply(density_gct, MARGIN = 1:2, mean)
     
     #Panel Layout: 6 rows by 3 columns, first 5 rows are spatial distributions
     #for the 15 species and the last row (2nd column) is for the inset.
-    par(mar = c(0,0,0,0), oma = c(1,1,1,1))
-    layout(mat = matrix(1:18, ncol = 3, byrow = T), heights = c(1,1,1,1,1,1.5))
-
-    for(ispp in 1:ns){     #For Each species
+    par(mar = c(0,0,0,0), oma = c(2,1,1,1))
+    layout(mat = matrix(c(1:15,18,17,16), ncol = 3, byrow = T))
+    
+    for(ispp in 1:(ns+1)){     #For Each species
         
         #Base Plot
         plot(1, type = 'n', axes = F, ann = F,
@@ -117,47 +117,60 @@ mean_density_gc = apply(density_gct, MARGIN = 1:2, mean)
         box()
         
         #Extract Data for a species
-        temp = data.frame(mean = mean_density_gc[,ispp])
+        if(ispp %in% 1:ns) temp = data.frame(mean = mean_density_gc[,ispp])
+        if(ispp == 16) temp = data.frame(mean = Extrapolation_depths$depth)
         
         #Discretize by decile: the first bin is for densities < 1, then the 
         #densities > 1 are broken up into 9 quantile groups. In total, there 
         #are 10 density bins. This was done so that the color scales aren't 
         #skewed too much by very low and very high values.
-        temp_int = as.integer(
-            cut(x = temp$mean, 
-                breaks = c(0,1,quantile(temp$mean[temp$mean > 1], 
-                                        probs = seq(0,1,length=10))[-1] )))
+        if(ispp %in% 1:ns){
+            temp_int = as.integer(
+                cut(x = temp$mean, 
+                    breaks = c(0,1,quantile(temp$mean[temp$mean > 1], 
+                                            probs = seq(0,1,length=10))[-1])))
+        }
+        
+        if(ispp == 16){
+            temp_int = as.integer(
+                cut(x = temp$mean, include.lowest = T,
+                    breaks = quantile(temp$mean, 
+                                      probs = seq(0,1,length=11) )))
+        }
         
         #Spatial Object: we create a raster by creating a spatial object from
         #the positions of the Extrapolation grid, then attributing the density
         #values across the grid. Then that object is rasterized.
-        goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km', 
-                                                                      'N_km')], 
+        goa = SpatialPointsDataFrame(coords = Extrapolation_depths[,c('E_km',
+                                                                      'N_km')],
                                      data = data.frame(val = temp_int))
         
         goa_ras = raster(goa, resolution = 5)
         goa_ras = rasterize(x = goa, y = goa_ras, field = 'val')
-    
+        
         #Plot density raster
-        image(goa_ras, col = c('white', brewer.pal(n = 9, name = 'Blues')),
+        color_density = ifelse(ispp %in% 1:ns, 'Blues', 'OrRd')
+        image(goa_ras, col = c('white', brewer.pal(n=9, name=color_density)),
               axes = F, asp = 1, ann = F, add = T)
+        
+        #Overlay 200 m isopleth
+        lwd_density = ifelse(ispp %in% 1:ns, 0.5, 2)
+        plot(bathy_iso_200, add = T, lty = 1, col = 'black', lwd = 0.5)
         
         #Isopleth legend
         legend(x = bbox_[1] + xrange*0.575,
                y = bbox_[3] + yrange*0.8,
                legend = '200 m\nisopleth', 
-               cex = 0.85, lty = 1, bty = 'n', lwd = 2)
+               cex = 0.75, lty = 1, bty = 'n', lwd = 2, col = 'black')
         
         #Overlay Land
         plot(AK, add = T, col = 'tan', border = F)
         
-        #Overlay 200 m isobar
-        plot(bathy_iso_200, add = T, lty = 1, col = 'black')
-        
         #Overlay Species Name
         text(x = bbox_[1] + xrange*0.15, 
              y = bbox_[3] + yrange*0.55,
-             gsub(Save$Spp[ispp], pattern = ' ', replacement = '\n'), font = 3)
+             gsub(sci_names[ispp], pattern = ' ', replacement = '\n'), 
+             font = ifelse(ispp %in% 1:ns, 3, 1))
         
         #Overlay scale bar
         segments(x0 = bbox_[1] , x1 = bbox_[1] + 500,
@@ -173,17 +186,32 @@ mean_density_gc = apply(density_gct, MARGIN = 1:2, mean)
              '500 km', cex = 0.8)
         
         #Create legend values
-        legend_vals = list(
-            top = c(
-                "< 1",
+        if(ispp %in% 1:ns){
+            legend_vals = list(
+                top = c(
+                    "< 1",
                     round(quantile(temp$mean[temp$mean > 1],
                                    probs = seq(0,1,length=10)))[c(NA,3,NA, 5,
                                                                   NA, 7,NA, 
                                                                   9,NA)]),
-            bottom = c(round(quantile(temp$mean[temp$mean > 1],
-                                      probs = seq(0,1,length=10)))[c(NA,2,NA,4,
-                                                                     NA,6,NA,8,
-                                                                     NA,10)]) )
+                bottom = c(round(quantile(temp$mean[temp$mean > 1],
+                                          probs = seq(0,1,length=10)))[c(NA,2,NA,4,
+                                                                         NA,6,NA,8,
+                                                                         NA,10)]) )
+        }
+        
+        if(ispp == 16) {
+            legend_vals = list(
+                top = round(quantile(temp$mean[temp$mean > 1],
+                                     probs = seq(0,1,length=10)))[c(1,NA,3,NA, 5,
+                                                                    NA, 7,NA, 
+                                                                    9,NA)],
+                bottom = round(quantile(temp$mean[temp$mean > 1],
+                                          probs = seq(0,1,length=10)))[c(NA,2,NA,4,
+                                                                         NA,6,NA,8,
+                                                                         NA,10)])
+        }
+        
         #Overlay Legend
         plotrix::color.legend(
             xl = bbox_[1] + xrange*0.4,
@@ -191,7 +219,7 @@ mean_density_gc = apply(density_gct, MARGIN = 1:2, mean)
             yb = bbox_[3] + yrange*0.05,
             yt = bbox_[3] + yrange*0.15,
             legend = legend_vals[['top']],
-            rect.col = c('white', brewer.pal(n = 9, name = 'Blues')), 
+            rect.col = c('white', brewer.pal(n = 9, name = color_density)), 
             cex = 0.5, align = 'rb' )
         plotrix::color.legend(
             xl = bbox_[1] + xrange*0.4,
@@ -199,24 +227,22 @@ mean_density_gc = apply(density_gct, MARGIN = 1:2, mean)
             yb = bbox_[3] + yrange*0.05,
             yt = bbox_[3] + yrange*0.15,
             legend = legend_vals[['bottom']],
-            rect.col = c('white', brewer.pal(n = 9, name = 'Blues')), 
+            rect.col = c('white', brewer.pal(n = 9, name = color_density)), 
             cex = 0.5, align = 'lt' )
         
     }
     
-    #Empty Plot
-    plot(1, type = 'n', axes = F, ann = F)
-    
     #Plot Inset
-    par(mar = c(1,0,0,0))
+    par(mar = c(0,1,0,1))
     mapPlot(coastlineCut(coastlineWorldFine, -135),
-            longitudelim=c(-180, -70), latitudelim=c(20, 65),
-            proj="+proj=stere +lat_0=30 +lon_0=-130", col='tan',
-            border = 'black' )
-    rect(xleft = -2800000, xright = 200000,
-         ybottom = 2600000, ytop = 3600000,
+            longitudelim=c(-180, -70), latitudelim=c(30, 60),
+            proj="+proj=aea +lat_0=40 +lon_0=-135", col='tan',
+            border = 'black', grid = c(15,15))
+    rect(xleft = -2000000, xright = 100000,
+         ybottom = 1600000, ytop = 2600000,
          lwd = 3)
-
+    
+    
     #Empty Plot
     plot(1, type = 'n', axes = F, ann = F)
     
