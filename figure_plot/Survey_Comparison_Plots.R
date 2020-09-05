27f6941e3927ed829c6a1b8244ac211fd252f448
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-09-05 15:36:02 -0700

    updated

--- figure_plot/Survey_Comparison_Plots.R
+++ figure_plot/Survey_Comparison_Plots.R
@@ -7,7 +7,7 @@ rm(list = ls())
 ## Set up directories
 #######################################
 
-which_machine = c('Zack_MAC' = 1, 'Zack_PC' = 2)[2]
+which_machine = c('Zack_MAC' = 1, 'Zack_PC' = 2, 'Zack_GI_PC' = 3)[3]
 VAST_model = "11" 
 
 github_dir = paste0(c('/Users/zackoyafuso/Documents/', 
