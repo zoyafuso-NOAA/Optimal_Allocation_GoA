85454a226df57a10bda9ef0f9d77c529fd2bc128
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-10-18 14:02:22 -0700

    updated

--- Survey_Optimization.R
+++ Survey_Optimization.R
@@ -66,7 +66,7 @@ ns <- c(15, 1)[which_method]
 ####   If Single_Species: subset just the one species
 ##################################################
 if (which_method == 2) {
-  SS_which_species <- 1 #which species are we doing?
+  SS_which_species <- 7 #which species are we doing?
   
   frame <- frame[,c("id", "X1", "X2", paste0("Y", SS_which_species),
                     "domainvalue")]
@@ -103,7 +103,7 @@ for (istrata in 2) {
   isample <- 1
   current_n <- 0
   
-  CV_constraints <- Current_STRS_Pop_CV[SS_which_species, 1] * 1
+  CV_constraints <- Current_STRS_Pop_CV[SS_which_species, 1] * 0.5
   
   ##Initial Upper CV constraints
   # if (VAST_model %in% c(paste0(10, letters[1:4]), '11') ) {
