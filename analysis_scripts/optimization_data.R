73d72785d4952d0f6bfda81db83793f0f814afb2
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2020-12-09 13:16:38 -0800

    lower- and snake_cased variable names, extended capabilities for data by district

--- analysis_scripts/optimization_data.R
+++ analysis_scripts/optimization_data.R
@@ -14,7 +14,7 @@ rm(list = ls())
 ##################################################
 ####    Set up directories here first 
 ##################################################
-which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[1]
+which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[3]
 
 github_dir <- paste0(c("/Users/zackoyafuso/Documents",
                        "C:/Users/Zack Oyafuso/Documents",
@@ -33,12 +33,12 @@ load(paste0(github_dir, "/data/Extrapolation_depths.RData"))
 ##################################################
 
 ## Years to use
-Year_Set <- 1996:2019
-Years2Include <- c(1, 4, 8, 10, 12, 14, 16, 18, 20, 22, 24)
-NTime <- length(Years2Include)
+year_set <- 1996:2019
+years_included <- c(1, 4, 8, 10, 12, 14, 16, 18, 20, 22, 24)
+n_years <- length(years_included)
 
 ## Number of sampling grids
-N <- nrow(Extrapolation_depths)
+n_cells <- nrow(Extrapolation_depths)
 
 ## Scientific and common names used in optimization
 sci_names_opt <- c("Atheresthes stomias", "Gadus chalcogrammus",
@@ -62,11 +62,10 @@ ns_opt <- length(sci_names_opt)
 ## Scientific and common names not used in optimization, but evaluated
 ## when simulating surveys
 
-sci_names_eval <- c(  "Anoplopoma fimbria", "Beringraja spp.", 
-                      "Octopus spp.", "Pleurogrammus monopterygius",
-                      "Sebastes borealis",
-                      # "Sebastes ruberrimus",
-                      "Sebastes variegatus", "Squalus suckleyi")
+sci_names_eval <- c("Anoplopoma fimbria", "Beringraja spp.", "Octopus spp.",
+                    "Pleurogrammus monopterygius", "Sebastes borealis",
+                    # "Sebastes ruberrimus",
+                    "Sebastes variegatus", "Squalus suckleyi")
 
 common_names_eval <- c("sablefish", "skates spp.", "Octopus spp.", 
                        "Atka mackerel", "shortraker rockfish",
@@ -80,6 +79,7 @@ sci_names_all <- sort(c(sci_names_opt, sci_names_eval))
 common_names_all <- c(common_names_opt, 
                       common_names_eval)[order(c(sci_names_opt, 
                                                  sci_names_eval))]
+
 ns_all <- ns_opt + ns_eval
 
 spp_idx_opt <- which(sci_names_all %in% sci_names_opt)
@@ -87,11 +87,11 @@ spp_idx_eval <- which(sci_names_all %in% sci_names_eval)
 
 ## Sample sizes across 1, 2, and 3 boats
 samples <- c(280, 550, 820)
-nboats <- length(samples)
+n_boats <- length(samples)
 
 ## Number of strata to input into optimization
 stratas <- c(10, 15, 20)
-NStrata <- length(stratas)
+n_strata <- length(stratas)
 
 ## Specify Management Districts
 districts <- data.frame("reg_area" = c("WRA", "CRA", 
@@ -101,16 +101,16 @@ districts <- data.frame("reg_area" = c("WRA", "CRA",
                         "domainvalue" = 1:5,
                         "W_lon" = c(-170, -159, -154, -147, -140),
                         "E_lon" = c(-159, -154, -147, -140, -132))
-ndom <- nrow(districts)
+n_dom <- nrow(districts)
 
-district_vals = cut(x = Extrapolation_depths$Lon, 
-                    breaks = c(-170, -159, -154, -147, -140, -132), 
-                    labels = 1:5)
+district_vals <- cut(x = Extrapolation_depths$Lon, 
+                     breaks = c(-170, -159, -154, -147, -140, -132), 
+                     labels = 1:5)
 
 ## Number of times to simulate survey
-Niters <- 1000
-obs_CV <- c(0, 0.1, 0.25, 0.5, 1) #low to high sampling CVs
-nobs_CV <- length(obs_CV)
+n_iters <- 1000
+obs_cv <- c(0, 0.1, 0.25, 0.5, 1) #low to high sampling CVs
+n_obs_cv <- length(obs_cv)
 
 ##################################################
 ####   Our df will have fields for:
@@ -128,55 +128,80 @@ nobs_CV <- length(obs_CV)
 ####   Y1_SQ_SUM, Y2_SQ_SUM, ... : density-squared for a given cell, 
 ####           summed across observed years
 ##################################################
-frame <- cbind(data.frame(domainvalue = 1,
-                          id = 1:N,
-                          X1 = with(Extrapolation_depths, E_km - min(E_km)),
-                          X2 = Extrapolation_depths$DEPTH_EFH,
-                          WEIGHT = NTime),
-               
-               matrix(data = apply(X = D_gct[, , Years2Include],
-                                   MARGIN = c(1, 2), 
-                                   FUN = sum),
-                      ncol = ns_all,
-                      dimnames = list(NULL, paste0("Y", 1:ns_all))),
-               
-               matrix(data = apply(X = D_gct[, , Years2Include],
-                                   MARGIN = c(1, 2), 
-                                   FUN = function(x) sum(x^2)),
-                      ncol = ns_all,
-                      dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
+frame_all <- cbind(data.frame(domainvalue = 1,
+                              id = 1:n_cells,
+                              X1 = with(Extrapolation_depths, E_km - min(E_km)),
+                              X2 = Extrapolation_depths$DEPTH_EFH,
+                              WEIGHT = n_years),
+                   
+                   matrix(data = apply(X = D_gct[, , years_included],
+                                       MARGIN = c(1, 2), 
+                                       FUN = sum),
+                          ncol = ns_all,
+                          dimnames = list(NULL, paste0("Y", 1:ns_all))),
+                   
+                   matrix(data = apply(X = D_gct[, , years_included],
+                                       MARGIN = c(1, 2), 
+                                       FUN = function(x) sum(x^2)),
+                          ncol = ns_all,
+                          dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
+)
+
+frame_district <- cbind(data.frame(
+  domainvalue = cut(x = Extrapolation_depths$Lon, 
+                    breaks = c(-170, -159, -154, -147, -140, -132), 
+                    labels = 1:5),
+  id = 1:n_cells,
+  X1 = with(Extrapolation_depths, E_km - min(E_km)),
+  X2 = Extrapolation_depths$DEPTH_EFH,
+  WEIGHT = n_years),
+  
+  matrix(data = apply(X = D_gct[, spp_idx_opt, years_included],
+                      MARGIN = c(1, 2), 
+                      FUN = sum),
+         ncol = ns_opt,
+         dimnames = list(NULL, paste0("Y", 1:ns_opt))),
+  
+  matrix(data = apply(X = D_gct[, spp_idx_opt, years_included],
+                      MARGIN = c(1, 2), 
+                      FUN = function(x) sum(x^2)),
+         ncol = ns_opt,
+         dimnames = list(NULL, paste0("Y", 1:ns_opt, "_SQ_SUM")))
 )
 
 ##################################################
-####   Calculate "true" mean density and "true" abundance index
+####   Calculate true mean density and true abundance index along with
+####   the true abundance index within districts 
 ##################################################
-true_mean <- apply(X = D_gct[,,Years2Include], 
+true_mean <- apply(X = D_gct[, , years_included], 
                    MARGIN = 2:3,
                    FUN = mean)
 
-true_index <- apply(X = Index[,, Years2Include], 
+true_index <- apply(X = Index[, , years_included], 
                     MARGIN = 2:3,
                     FUN = sum)
 
-true_index_district <- apply(X = Index[,, Years2Include], 
+true_index_district <- apply(X = Index[,, years_included], 
                              MARGIN = 2:3,
                              FUN = function(x) tapply(x, 
                                                       INDEX = district_vals, 
                                                       FUN = sum))
-true_index_district <- aperm(a = true_index_district, perm = c(2,3,1))
+
+true_index_district <- aperm(a = true_index_district, 
+                             perm = c(2,3,1))
 
 ##################################################
 ####   Save Data
 ##################################################
-save(list = c("frame", 
-              "districts", "district_vals", "ndom",
+save(list = c("frame_all", "frame_district",
+              "districts", "district_vals", "n_dom",
               "true_mean", "true_index", "true_index_district",
               "ns_all", "ns_eval", "ns_opt", 
               "common_names_all", "common_names_eval", "common_names_opt",
               "sci_names_all", "sci_names_eval", "sci_names_opt",
               "spp_idx_eval", "spp_idx_opt",
-              "Year_Set", "Years2Include", "NTime", 
-              "N", "samples", "nboats", "Niters", 
-              "obs_CV", "nobs_CV",
-              "stratas", "NStrata"),
+              "year_set", "years_included", "n_years", 
+              "n_cells", "samples", "n_boats", "n_iters", 
+              "obs_cv", "n_obs_cv",
+              "stratas", "n_strata"),
      file = paste0(github_dir, "data/optimization_data.RData"))
