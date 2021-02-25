e97c084de3f4d321cedcbf5e4f1c8fcacfaa1d0e
zoyafuso--NOAA <zack.oyafuso@noaa.gov>
2021-02-24 17:44:08 -0800

    changed common names, added sample sizes, added inpfc strata

--- analysis_scripts/optimization_data.R
+++ analysis_scripts/optimization_data.R
@@ -14,7 +14,7 @@ rm(list = ls())
 ##################################################
 ####    Set up directories here first 
 ##################################################
-which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[3]
+which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[2]
 
 github_dir <- paste0(c("/Users/zackoyafuso/Documents",
                        "C:/Users/Zack Oyafuso/Documents",
@@ -49,11 +49,11 @@ sci_names_opt <- c("Atheresthes stomias", "Gadus chalcogrammus",
                    "Sebastes brevispinis", "Sebastes polyspinis", 
                    "Sebastes variabilis", "Sebastolobus alascanus" )
 
-common_names_opt <- c("arrowtooth flounder", "Alaska pollock", "Pacific cod",
+common_names_opt <- c("arrowtooth flounder", "walleye pollock", "Pacific cod",
                       "rex sole", "flathead sole", "Pacific halibut", 
                       "southern rock sole", "northern rock sole", 
-                      "Pacific Dover sole", "Pacific ocean perch", 
-                      "blackspotted/rougheye\nrockfishes", 
+                      "Dover sole", "Pacific ocean perch", 
+                      "BS and RE rockfishes", 
                       "silvergrey rockfish", "northern rockfish", 
                       "dusky rockfish", "shortspine thornyhead")
 
@@ -64,21 +64,23 @@ ns_opt <- length(sci_names_opt)
 
 sci_names_eval <- c("Anoplopoma fimbria", "Beringraja spp", "Octopus spp",
                     "Pleurogrammus monopterygius", "Sebastes borealis",
-                    # "Sebastes ruberrimus",
                     "Sebastes variegatus", "Squalus suckleyi")
 
-common_names_eval <- c("sablefish", "skates spp.", "Octopus spp.", 
+common_names_eval <- c("sablefish", "skates spp", "Octopus spp", 
                        "Atka mackerel", "shortraker rockfish",
-                       # "yelloweye rockfish",
-                       "harlequin rockfish", "spiny dogfish")
+                       "harlequin rockfish", "Pacific spiny dogfish")
+common_names_eval_labels <- c("sablefish", "skates spp.", "Octopus spp.", 
+                              "Atka mackerel", "shortraker rockfish",
+                              "harlequin rockfish", "Pacific spiny dogfish")
 
 ns_eval <- length(sci_names_eval)
 
 ## In case we need it, all species names together
-sci_names_all <- sort(c(sci_names_opt, sci_names_eval))
+sci_names_all <- c(sci_names_opt, sci_names_eval)
 common_names_all <- c(common_names_opt, 
-                      common_names_eval)[order(c(sci_names_opt, 
-                                                 sci_names_eval))]
+                      common_names_eval)
+common_names_all_labels <- c(common_names_opt, 
+                             common_names_eval_labels)
 
 ns_all <- ns_opt + ns_eval
 
@@ -86,7 +88,7 @@ spp_idx_opt <- which(sci_names_all %in% sci_names_opt)
 spp_idx_eval <- which(sci_names_all %in% sci_names_eval)
 
 ## Sample sizes across 1, 2, and 3 boats
-samples <- c(280, 550, 820)
+samples <- c(292, 550, 825)
 n_boats <- length(samples)
 
 ## Number of strata to input into optimization
@@ -96,8 +98,8 @@ n_strata <- length(stratas)
 ## Specify Management Districts
 districts <- data.frame("reg_area" = c("WRA", "CRA", 
                                        "CRA", "ERA", "ERA"),
-                        "district" = c("W", "Chirikof", 
-                                       "Kodiak", "Yakutat", "SE"),
+                        "district" = c("West", "Chirikof", 
+                                       "Kodiak", "Yakutat", "Southeast"),
                         "domainvalue" = 1:5,
                         "W_lon" = c(-170, -159, -154, -147, -140),
                         "E_lon" = c(-159, -154, -147, -140, -132))
@@ -111,6 +113,19 @@ districts[, c("W_UTM", "E_UTM")] <-
   do.call(rbind,tapply(X = Extrapolation_depths$E_km, 
                        INDEX = district_vals, 
                        FUN = range) )
+
+## International North Pacific Fisheries Commission statistical areas
+inpfc_vals_current <- district_vals
+inpfc_vals_current[Extrapolation_depths$stratum %in% 
+                        c(10:13, 110:112, 210, 310, 410, 510)] <- 1
+inpfc_vals_current[Extrapolation_depths$stratum %in% 
+                        c(20:22, 120:122, 220:221, 320, 420, 520)] <- 2
+inpfc_vals_current[Extrapolation_depths$stratum %in% 
+                        c(30:33, 35, 130:134, 230:232, 330, 430, 530)] <- 3
+inpfc_vals_current[Extrapolation_depths$stratum %in% 
+                        c(40:41, 140:143, 240:241, 340:341, 440, 540)] <- 4
+inpfc_vals_current[Extrapolation_depths$stratum %in% 
+                        c(50, 150:151, 250:251, 350:351, 450, 550)] <- 5
 
 ## Number of times to simulate survey
 n_iters <- 500
@@ -192,7 +207,6 @@ true_index_district <- apply(X = Index[,, years_included],
                              FUN = function(x) tapply(x, 
                                                       INDEX = district_vals, 
                                                       FUN = sum))
-
 true_index_district <- aperm(a = true_index_district, 
                              perm = c(2,3,1))
 
@@ -200,10 +214,11 @@ true_index_district <- aperm(a = true_index_district,
 ####   Save Data
 ##################################################
 save(list = c("frame_all", "frame_district",
-              "districts", "district_vals", "n_dom",
+              "districts", "district_vals", "n_dom", "inpfc_vals_current",
               "true_mean", "true_index", "true_index_district",
               "ns_all", "ns_eval", "ns_opt", 
               "common_names_all", "common_names_eval", "common_names_opt",
+              "common_names_eval_labels", "common_names_all_labels",
               "sci_names_all", "sci_names_eval", "sci_names_opt",
               "spp_idx_eval", "spp_idx_opt",
               "year_set", "years_included", "n_years", 
