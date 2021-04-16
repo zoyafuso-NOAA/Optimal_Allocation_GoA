4fcf0e0eedf7bac243a0d23c34c67d2366e25c93
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2021-04-15 23:30:53 -0700

    updated

--- analysis_scripts/knitting_runs.R
+++ analysis_scripts/knitting_runs.R
@@ -9,7 +9,7 @@ rm(list = ls())
 ##################################################
 ####  Set up directories
 ##################################################
-which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[2]
+which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2, "Zack_GI_PC" = 3)[3]
 
 github_dir <- paste0(c("/Users/zackoyafuso/Documents/", 
                        "C:/Users/Zack Oyafuso/Documents/",
