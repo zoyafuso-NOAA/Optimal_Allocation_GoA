1eaf2c9ed915ae0f096455d816e5feea34031923
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2021-10-07 17:53:01 -0700

    added different data types

--- analysis_scripts/2A_optimization_data.R
+++ analysis_scripts/2A_optimization_data.R
@@ -20,6 +20,12 @@ load("data/processed/prednll_VAST_models.RData")
 load("data/processed/grid_goa.RData")
 
 ##################################################
+####   Location of VAST result output
+##################################################
+library(VAST)
+VAST_sim_data_dir <- "E:/VAST_Runs/"
+
+##################################################
 ####   Constants used throughout all scripts
 ##################################################
 
@@ -49,8 +55,8 @@ spp_idx_eval <- (1:ns_all)[-spp_idx_opt]
 common_names_eval <- common_names_all[spp_idx_eval]
 ns_eval <- length(common_names_eval)
 
-## Sample sizes across 1, 2, and 3 boats
-samples <- c(292, 550, 825)
+## Sample sizes across 2 boats
+samples <- c(292, 550, 825)[2]
 n_boats <- length(samples)
 
 ## Specify Management Districts
@@ -95,10 +101,10 @@ n_iters <- 1000
 ####   Our df will have fields for:
 ####   domain: only one domain so the value is just 1
 ####   id: unique ID for each sampling cell
-####   X1: strata variable 1: longitude in eastings (km). Because the 
-####           optimization does not read in negative values, I shift the 
-####           values so that the lowest value is 0
-####   X2: strata variable 2: depth of cell (m)
+####   X1: strata variable 2: depth of cell (m) 
+####   X2: strata variable 1: longitude in eastings (km). Because the 
+####       optimization does not read in negative values, I shift the 
+####       values so that the lowest value is 0
 ####
 ####   Variables used to more efficiently calcualte stratum variance 
 ####
@@ -107,47 +113,47 @@ n_iters <- 1000
 ####   Y1_SQ_SUM, Y2_SQ_SUM, ... : density-squared for a given cell, 
 ####           summed across observed years
 ##################################################
-frame_all <- cbind(
-  data.frame(domainvalue = 1,
-             id = 1:n_cells,
-             X1 = with(grid_goa, E_km - min(E_km)),
-             X2 = grid_goa$DEPTH_EFH,
-             WEIGHT = n_years),
-  
-  matrix(data = apply(X = D_gct,
-                      MARGIN = c(1, 2), 
-                      FUN = sum),
-         ncol = ns_all,
-         dimnames = list(NULL, paste0("Y", 1:ns_all))),
-  
-  matrix(data = apply(X = D_gct,
-                      MARGIN = c(1, 2), 
-                      FUN = function(x) sum(x^2)),
-         ncol = ns_all,
-         dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
-)
-
-frame_district <- cbind(data.frame(
-  domainvalue = cut(x = grid_goa$Lon, 
-                    breaks = c(-170, -159, -154, -147, -140, -132), 
-                    labels = 1:5),
-  id = 1:n_cells,
-  X1 = with(grid_goa, E_km - min(E_km)),
-  X2 = grid_goa$DEPTH_EFH,
-  WEIGHT = n_years),
-  
-  matrix(data = apply(X = D_gct,
-                      MARGIN = c(1, 2), 
-                      FUN = sum),
-         ncol = ns_all,
-         dimnames = list(NULL, paste0("Y", 1:ns_all))),
-  
-  matrix(data = apply(X = D_gct,
-                      MARGIN = c(1, 2), 
-                      FUN = function(x) sum(x^2)),
-         ncol = ns_all,
-         dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
-)
+# frame_all <- cbind(
+#   data.frame(domainvalue = 1,
+#              id = 1:n_cells,
+#              X1 = grid_goa$DEPTH_EFH,
+#              X2 = with(grid_goa, E_km - min(E_km)),
+#              WEIGHT = n_years),
+#   
+#   matrix(data = apply(X = D_gct,
+#                       MARGIN = c(1, 2), 
+#                       FUN = sum),
+#          ncol = ns_all,
+#          dimnames = list(NULL, paste0("Y", 1:ns_all))),
+#   
+#   matrix(data = apply(X = D_gct,
+#                       MARGIN = c(1, 2), 
+#                       FUN = function(x) sum(x^2)),
+#          ncol = ns_all,
+#          dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
+# )
+# 
+# frame_district <- cbind(data.frame(
+#   domainvalue = cut(x = grid_goa$Lon, 
+#                     breaks = c(-170, -159, -154, -147, -140, -132), 
+#                     labels = 1:5),
+#   id = 1:n_cells,
+#   X1 = grid_goa$DEPTH_EFH,
+#   X2 = with(grid_goa, E_km - min(E_km)),
+#   WEIGHT = n_years),
+#   
+#   matrix(data = apply(X = D_gct,
+#                       MARGIN = c(1, 2), 
+#                       FUN = sum),
+#          ncol = ns_all,
+#          dimnames = list(NULL, paste0("Y", 1:ns_all))),
+#   
+#   matrix(data = apply(X = D_gct,
+#                       MARGIN = c(1, 2), 
+#                       FUN = function(x) sum(x^2)),
+#          ncol = ns_all,
+#          dimnames = list(NULL, paste0("Y", 1:ns_all, "_SQ_SUM")))
+# )
 
 ##################################################
 ####   Calculate true mean density and true abundance index along with
@@ -188,17 +194,82 @@ scenarios <- data.frame(
   max_depth = c(rep(x = 1000, times = 9), rep(x = 700, times = 2)),
   stringsAsFactors = FALSE)
 
+##################################################
+####   Loop over species and simulate data with the four different options
+####   in the FishStatsUtils::simulate_data() function
+##################################################
+dens_vals <- array(dim = c(n_cells, ns_opt, n_years, 3),
+                   dimnames = list(NULL, common_names_opt, NULL, 
+                                   c("MLE", "measurement", "fixed_random")))
+
+for (itype in c("MLE", "measurement", "fixed_random")) {
+  for (ispp in common_names_opt) {
+    
+    pred_jnll_score <- subset(x = pred_jnll, 
+                              spp_name == ispp, 
+                              select = c("FALSE", "TRUE"))
+    depth_in_model <- as.logical(names(which.min(pred_jnll_score)))
+    
+    temp_VAST_dir <- paste0(VAST_sim_data_dir, ispp, 
+                            ifelse(depth_in_model == TRUE,
+                                   yes = "_depth", no = ""), "/")
+    
+    load(paste0(temp_VAST_dir, "fit_sim.RData"))
+    
+    dyn.load(paste0(temp_VAST_dir, "VAST_v12_0_0.dll"))
+    
+    if(itype == "MLE") {
+      dens_vals[, ispp, , "MLE"] <- 
+        fit_sim$Report$D_gct[, 1, years_included]
+    }
+    
+    if(itype != "MLE") {
+      
+      sim_val <- FishStatsUtils::simulate_data(
+        fit = fit_sim,
+        type = c("measurement" = 1,
+                 "fixed_random" = 3)[itype],
+        random_seed = 123423)
+      
+      pred_TF <- (1:length(sim_val$b_i))[-c(1:7900)]
+      
+      temp_density <- matrix(data = sim_val$b_i[pred_TF],
+                             nrow = n_cells,
+                             ncol = n_years)
+      
+      ## Temporary input objects for the survey simulation
+      temp_density <- sweep(x = temp_density,
+                            MARGIN = 1,
+                            STATS = grid_goa$Area_km2,
+                            FUN = "/")
+      
+      dens_vals[, ispp, , itype] <- temp_density
+    }
+    
+    dyn.unload(paste0(temp_VAST_dir, "VAST_v12_0_0.dll"))
+    print(paste("Finished with Type", itype, ispp))
+  }
+  
+  #Save
+  
+  # save(dens_vals, 
+  #      file = paste0(github_dir, 
+  #                    "/results/sensitivity_analysis/dens_vals_type", itype,
+  #                    ".RData"))
+}
+
 
 ##################################################
 ####   Save Data
 ##################################################
-save(list = c("frame_all", "frame_district", "scenarios",
-              "districts", "district_vals", "n_districts", "inpfc_vals_current",
-              "true_mean", "true_index", "true_index_district",
-              "ns_all", "ns_eval", "ns_opt", 
-              "x_range", "y_range",
-              "common_names_all", "common_names_eval", "common_names_opt",
-              "spp_idx_eval", "spp_idx_opt",
-              "year_set", "years_included", "n_years", 
-              "n_cells", "samples", "n_boats", "n_iters"),
-     file = "data/processed/optimization_data.RData")
+save(list = c(#"frame_all", "frame_district", 
+  "scenarios", "dens_vals",
+  "districts", "district_vals", "n_districts", "inpfc_vals_current",
+  "true_mean", "true_index", "true_index_district",
+  "ns_all", "ns_eval", "ns_opt", 
+  "x_range", "y_range",
+  "common_names_all", "common_names_eval", "common_names_opt",
+  "spp_idx_eval", "spp_idx_opt",
+  "year_set", "years_included", "n_years", 
+  "n_cells", "samples", "n_boats", "n_iters"),
+  file = "data/processed/optimization_data.RData")
