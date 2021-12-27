82436e227824522fc31830603726991abc966824
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2021-12-27 08:00:34 -0800

    cleaned up, added more comments

--- products/STRS_Solutions.R
+++ products/STRS_Solutions.R
@@ -1,7 +1,9 @@
 ###############################################################################
-## Project:       Optimized Strata Boundaries
+## Project:       STRS Survey Design Optimization : Strata Boundaries
 ## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov)
-## Description:   Create excel worksheet 
+## Description:   Create excel worksheet with solution information and 
+##                    a shapefile with all the solutions for a subset of 
+##                    scenarios defined in the Tech Memo.
 ###############################################################################
 rm(list = ls())
 
@@ -12,7 +14,8 @@ library(raster)
 library(sp)
 library(rgdal)
 library(RColorBrewer)
-library(xlsx); library(openxlsx)
+library(xlsx)
+library(openxlsx)
 
 ##################################################
 ####   Import Data
@@ -37,9 +40,10 @@ scenarios$`stratum variables` <-
 
 scenarios$`Notes` <- 
   sapply(X = paste0(scenarios$`Notes`),
-         FUN = function(x) switch(x,
-                                  "300" = "depths > 300 m  are set to 300 m in optimization",
-                                  "1000" = ""))
+         FUN = function(x) 
+           switch(x,
+                  "300" = "depths > 300 m  are set to 300 m in optimization",
+                  "1000" = ""))
 
 ##################################################
 ####   Result Objects
@@ -53,43 +57,44 @@ strs_sols <- sp::SpatialPointsDataFrame(
   coords = grid_goa[, c("Lon", "Lat")], 
   proj4string = crs("+proj=longlat +datum=WGS84"), 
   data = grid_goa[, c("Lon", "Lat")])
-# strs_sols_list <- list()
 
 ##################################################
-####   Loop over scenarios, load, and pull out solution s
+####   Loop over scenarios, load, and pull out solutions and update the excel
+####        worksheet with the characteristics of each solution as well as a 
+####        low-res image of the solution
 ##################################################
 for (iscen in scen) {
+  
+  ## Load solution 
   load(paste0("results/scenario_", iscen, 
               "/Multispecies_Optimization/Str_5/boat_2/result_list.RData"))
   
+  ## "Subset out" the deep (> 700 m) cells
   result_list$sol_by_cell[!grid_goa$shallower_than_700m] <- 0 
   
+  ## Calculate strata areas
   strs_sols@data[, paste0("scenario_", iscen)] <- result_list$sol_by_cell
   stratum_area <- round(tapply(X = grid_goa$Area_km2, 
                                INDEX = result_list$sol_by_cell, 
                                FUN = sum))
   
+  ## Select strata characteristics 
   temp <- result_list$sum_stats[, c("Domain", "Allocation",
                                     "Lower_X1", "Upper_X1") ]
   temp$Domain <- districts$district[temp$Domain]
   names(temp)[3:4] <- c("lower_depth", "upper_depth")
   
-  # if (iscen %in% c("D", "J")) {
-  #   temp <- cbind(temp,
-  #                 result_list$sum_stats[, c("Lower_X2", "Upper_X2") ])
-  #   names(temp)[5:6] <-  c("lower_lon", "upper_lon")
-  # }
-  
   temp <- cbind(temp, 
                 total_area_km2 = stratum_area[names(stratum_area) != "0"])
   
+  ## Update excel worksheet
   xlsx::write.xlsx(x = temp, 
                    file = "products/STRS_Solutions.xlsx",
                    sheetName = paste0("scenario_", iscen), 
                    row.names = FALSE, 
                    append = TRUE)
   
-  ## Plot
+  ## Create plot of strata boundaries
   png(filename = paste0("products/scenario_", iscen, ".png"),
       res = 500, width = 8, height = 4, units = "in")
   par(mar = c(0, 0, 0, 0))
@@ -104,6 +109,7 @@ for (iscen in scen) {
   legend("bottom", legend = "**black indicates areas > 700 m", bty = "n")
   dev.off()
   
+  ## Update excel worksheet with image and save
   wb <- loadWorkbook(file = "products/STRS_Solutions.xlsx")
   img <- system.file("extdata", 
                      paste0("products/scenario_", iscen, ".png"), 
@@ -116,5 +122,13 @@ for (iscen in scen) {
   
 }
 
+##################################################
+####   Transform shapefile to aea and save
+##################################################
 strs_sols <- sp::spTransform(x = strs_sols,
                              CRSobj = crs(ak_land))
+
+writeOGR(obj = strs_sols, 
+         dsn = "products/STRS_sols.shp", 
+         driver = "ESRI Shapefile",
+         layer = "strs_sols")
